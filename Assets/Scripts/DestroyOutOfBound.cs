402b3c208161abc94f957b99a0aa3ec7384d1938
Kemrode <kevinfichou@gmail.com>
2023-04-08 17:08:20 +0200

    feat(master): first commit of the project

---
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBound : MonoBehaviour
+{
+    private float topBound = 30.0f;
+    private float bottomBound = -10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < bottomBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
